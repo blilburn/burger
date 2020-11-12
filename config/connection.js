adeb4396b60aee4aa14c5c5e23897fc425f510cf
Bryan Lilburn <bryanlilburn@gmail.com>
2020-11-11 22:35:53 -0800

    Updated connection JS

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,17 @@
 // Set up MySQL connection.
 const mysql = require("mysql");
 
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
 const connection = mysql.createConnection({
-  host: "localhost",
+  host: "nuskkyrsgmn5rw8c.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   port: 3306,
-  user: "root",
-  password: "root",
-  database: "burgers_db"
+  user: "zp97f78bmar8pcav",
+  password: "ewmbz9awa8mu3uwl",
+  database: "fr9uwkvogulg03lc"
 });
-
+};
 // Make connection.
 connection.connect(function(err) {
   if (err) {
